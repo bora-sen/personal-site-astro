42cf36826a80d81e4bb913c5208560bc2709aef6
bora-sen <bahattinborasen@gmail.com>
2023-06-01 14:47:32 +0300

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fdfcfb",
+        "primary-dark": "#010101",
+        paragraph: "#242ac2",
+        accentOrange: "#e81e6b",
+        accentGreen: "#188a0e",
+        accentAqua: "#242ac2",
+        accentRed: "#881a45",
+        "primary-blue": "#1b6cbb",
+        "primary-purple": "#a92e7a",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        tusker: ["TuskerRegular"],
+        "tusker-bold": ["TuskerBold"],
+      },
+    },
+  },
+  plugins: [],
+}
